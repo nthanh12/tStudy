4ec8de693d3c0b81f6a90de560048150802a7e00
nthanh12 <nguyenkimthanh25122002@gmail.com>
2024-11-12 00:05:13 +0700

    upcode 12-11: Create prj with EF Identity

---
+++ tStudy/Models/Data/tStudyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using tStudy.Models.Entities;
+
+namespace tStudy.Models.Data
+{
+    public class tStudyDbContext : IdentityDbContext<SystemUser, IdentityRole, string,
+            IdentityUserClaim<string>, IdentityUserRole<string>, IdentityUserLogin<string>,
+            IdentityRoleClaim<string>, IdentityUserToken<string>>
+    {
+        public tStudyDbContext(DbContextOptions<tStudyDbContext> dbOptions) : base(dbOptions) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ tStudy/Models/Entities/SystemUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace tStudy.Models.Entities
+{
+    public class SystemUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
